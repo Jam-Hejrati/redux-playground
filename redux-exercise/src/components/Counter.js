ed9fc3d9f2a42e0e3c9e91e378138cc84b634f7e
Jam <jamhejratipro@gmail.com>
2023-02-25 15:13:24 +0330

    add: attached an amount to increase action

--- redux-exercise/src/components/Counter.js
+++ redux-exercise/src/components/Counter.js
@@ -1,31 +1,37 @@
 import classes from './Counter.module.css';
-import {useDispatch , useSelector} from 'react-redux'
+import {useDispatch, useSelector} from 'react-redux'
 
 const Counter = () => {
-  const dispatch =  useDispatch()
-  const counter = useSelector(state => state.counter)
+    const dispatch = useDispatch()
+    const counter = useSelector(state => state.counter)
 
-  const incrementHandler = () => {
-    dispatch({type: 'increment'})
-  }
+    const increaseHandler = () => {
+        dispatch({type: 'increase', amount: 3});
+    }
 
-  const decrementHandler = () => {
-    dispatch({type: 'decrement'})
-  }
+    const incrementHandler = () => {
+        dispatch({type: 'increment'})
+    }
 
-  const toggleCounterHandler = () => {};
+    const decrementHandler = () => {
+        dispatch({type: 'decrement'})
+    }
 
-  return (
-    <main className={classes.counter}>
-      <h1>Redux Counter</h1>
-      <div className={classes.value}>{counter}</div>
-      <div>
-        <button onClick={incrementHandler}>Increment</button>
-        <button onClick={decrementHandler}>Decrement</button>
-      </div>
-      <button onClick={toggleCounterHandler}>Toggle Counter</button>
-    </main>
-  );
+    const toggleCounterHandler = () => {
+    };
+
+    return (
+        <main className={classes.counter}>
+            <h1>Redux Counter</h1>
+            <div className={classes.value}>{counter}</div>
+            <div>
+                <button onClick={incrementHandler}>Increment</button>
+                <button onClick={increaseHandler}>Increment by 3</button>
+                <button onClick={decrementHandler}>Decrement</button>
+            </div>
+            <button onClick={toggleCounterHandler}>Toggle Counter</button>
+        </main>
+    );
 };
 
 export default Counter;
