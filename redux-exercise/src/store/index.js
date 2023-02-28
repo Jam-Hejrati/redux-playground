d7ae27b1984e4809bc3a4b7b2c3d1b758bf14bd6
Jam <jamhejratipro@gmail.com>
2023-02-28 12:12:16 +0330

    format: formatted all files

--- redux-exercise/src/store/index.js
+++ redux-exercise/src/store/index.js
@@ -1,28 +1,28 @@
 import {configureStore, createSlice} from "@reduxjs/toolkit";
 
-const initialState = {counter: 0, showCounter: true}
+const initialCounterState = {counter: 0, showCounter: true}
 
 const counterSlice = createSlice({
-    name: 'counter',
-    initialState,
-    reducers: {
-        increment(state) {
-            state.counter++
-        },
-        decrement(state) {
-            state.counter--
-        },
-        increase(state , action) {
-            state.counter = state.counter + action.payload
-        },
-        toggleCounter(state) {
-            state.showCounter = !state.showCounter
-        }
+  name: 'counter',
+  initialState: initialCounterState,
+  reducers: {
+    increment(state) {
+      state.counter++
+    },
+    decrement(state) {
+      state.counter--
+    },
+    increase(state, action) {
+      state.counter = state.counter + action.payload
+    },
+    toggleCounter(state) {
+      state.showCounter = !state.showCounter
     }
+  }
 })
 
 const store = configureStore({
-    reducer: counterSlice.reducer
+  reducer: counterSlice.reducer
 })
 
 export const counterActions = counterSlice.actions
